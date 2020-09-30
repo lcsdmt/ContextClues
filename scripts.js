011d922a0d3276587cf28c798611588e95e2681a
lcsdmt <lcsdmt@users.noreply.github.com>
2020-09-30 12:08:19 -0500

    moved var to show I know how it works

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
-    var friends = ["Rick", "Fin", "Bensin", "Space Ghost", "Strider"];
-    var weapons = ["unicycle", "Bagel Bites", "lazer gun", "rusty spoon", "covid", "lightsaber", "raw steak", "pineapple", "fitted sheet", "door knob", "latex glove", "mop water", "grandma", "russian doll", "a picture of billy joel", "Keshas song TikTok", "my uncles bitty shoe", "a vhs copy of DieHard", "a plague of squirrels", "Zoidberg"];
-    var places = ["the lot behind waffle house", "suburban Ontario", "North Korea", "the upsidedown", "my basement", "the local trailer park, in the RV named: Grrrrr", "the room behind josh, while doing the JustMyType walkthrough", "their dogs favorite spot to pee", "Ted Dansons mind", "the backstreet that gave us the boys"];
+    friends = ["Rick", "Fin", "Bensin", "Space Ghost", "Strider"];
+    weapons = ["unicycle", "Bagel Bites", "lazer gun", "rusty spoon", "covid", "lightsaber", "raw steak", "pineapple", "fitted sheet", "door knob", "latex glove", "mop water", "grandma", "russian doll", "a picture of billy joel", "Keshas song TikTok", "my uncles bitty shoe", "a vhs copy of DieHard", "a plague of squirrels", "Zoidberg"];
+    places = ["the lot behind waffle house", "suburban Ontario", "North Korea", "the upsidedown", "my basement", "the local trailer park, in the RV named: Grrrrr", "the room behind josh, while doing the JustMyType walkthrough", "their dogs favorite spot to pee", "Ted Dansons mind", "the backstreet that gave us the boys"];
 
     for (let i = 1; i < 101; i++) {
         createH3(i);
@@ -13,4 +13,7 @@ $(document).ready(function () {
         })
         $('body').append(accusationH3);
     }
+    var friends;
+    var weapons;
+    var places;
 });
